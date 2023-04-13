e3b44e7700de6b1987339d49b52d72242a2ec4db
kalebbroo <kalebbroo@gmail.com>
2023-04-13 01:38:58 -0400

    added config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+## Config options. Enter your Discord bot and Plex information. ##
+
+## Discord Bot Token ##
+TOKEN = 'BOT_TOKEN'
+
+## Plex Username ##
+PLEX_USERNAME = 'USERNAME'
+
+## Plex Password ##
+PLEX_PASSWORD = 'PASSWORD'
+
+## Plex Token (This is a placeholder and is currently not used) ##
+PLEX_TOKEN = "TOKEN"
+
+## Plex Server Name ##
+SERVER = "SERVER"
