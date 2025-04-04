bbb6172ba1d227b427686cd8cba008263f6f129c
kalebbroo <kalebbroo@gmail.com>
2025-04-04 10:16:55 -0400

    Remove old files

--- Core/PlexAPI/PlexLibrary.cs
+++
@@ -1,8 +0,0 @@
-﻿
-
-namespace PlexBot.Core.PlexAPI;
-
-internal class PlexLibrary(ILogger<PlexLibrary> logger) : PlexCore(logger)
-{
-    // TODO: Add basic backend for other media in the library
-}
